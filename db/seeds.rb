eab318b3f665182b5d757bc97096747f944f59f4
Luke Hemingway <lukehemingway66@gmail.com>
2017-10-10 10:24:10 +0100

    images loading in circle images in the card

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 user1 = User.create(email: 'user@gmail.com', password: 'password')
 
-project1 = Project.create(screenshot: "Luke.jpg", description: "healthy", technologies: "burley", url: "http://fillmurray.com/300/300", public_private: true)
+project1 = Project.create(screenshot: "taco.jpg", description: "healthy", technologies: "burley", url: "http://fillmurray.com/300/300", public_private: true)
 project2 = Project.create(screenshot: "taco.jpg", description: "unhealthy", technologies: "headingley", url: "http://fillmurray.com/300/300", public_private: true)
 
 user1.projects << project1
