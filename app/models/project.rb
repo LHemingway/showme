64b3223934b23a24a5281f26a0fb1850d9cb35e8
Luke Hemingway <lukehemingway66@gmail.com>
2017-10-11 18:43:19 +0100

    Validation on the forms so that first name and stuff can't be left blank check the project and user model if you want to change what can and can't be left blank message me on slack if you need clarifcation or just ask ollie probably better

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ApplicationRecord
   has_many :users_projects
 	has_many :users, through: :users_projects
+
+	# This means that they have to be present i.e. not null!
+	# Slight problem (It creates a gap when between the label and text field when left null 
+	# and errors if you try to refresh without putting data in them)
+  validates :screenshot, :description, :technologies, :url, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
   has_many :users_projects
 	has_many :projects, through: :users_projects
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+	
+  # This means that they have to be present i.e. not null!
+  # Slight problem (It creates a gap when between the label and text field when left null 
+  # and errors if you try to refresh without putting data in them)
+  validates :first_names, :surname, :start_date, :course_path,  presence: true
 end
