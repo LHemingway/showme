e65477284fc5bd579b2978a4b9698e925bff07b4
Charlotte Leedham <c.leedham18@gmail.com>
2017-10-11 18:09:37 +0100

    Commenting in the code

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,7 @@ class ProjectsController < ApplicationController
   before_action :set_projects
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
+# if user is signed in it will display the current user or user is nil and it will just display projects
   def index
     if user_signed_in?
       @user = current_user
@@ -11,28 +12,32 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  # Find user params and find user projects
   # GET users/1/projects
   def index_user
     @user = User.find(params[:id])
     @projects = @user.projects
   end
 
+  #find user details and show projects for this user
   # GET users/1/projects/1
   def show
     @user = User.find(params[:user_id])
     @project = Project.find(params[:id])
   end
 
+  # Create new project and bring form up for the new project
   # GET users/1/projects/new
   def new
     @project = Project.new
   end
 
-  
-
+  # Create the new projects using the details from the populated form
   # POST users/1/projects
   def create
     @project = @user.projects.create(project_params)
+
+    # Save and redirect to user projects with a notice at the top saying that the project has been created
 
     if @project.save
       redirect_to(user_projects_path(@project), notice: 'Project was successfully created.')
@@ -41,19 +46,23 @@ class ProjectsController < ApplicationController
     end
   end
 
+# Brings up the form for the edit with the data pre populated as it is in the specific id of the project
 # GET users/1/projects/1/edit
   def edit
   end
   
+  # update using the project params in the populated form
   # PUT users/1/projects/1
   def update
     if @project.update_attributes(project_params)
+      #redirect to home with the notice that project has been successfully updated
       redirect_to "/", notice: 'Project was successfully updated.'
     else
       render action: 'edit'
     end
   end
 
+  # This deletes the project with the id that has been specified by clicking on the project
   # DELETE users/1/projects/1
   def destroy
     @project.destroy
