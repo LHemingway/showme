7a2c2ad65c1ef9048b56ba3438e9ed352b2fcf86
= <=>
2017-10-13 10:02:48 +0100

    sorted the admin rights; basic, still no show page of all users and to delete them

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
 
+# The show method here is linked to the show page where the spartan can view their own projects
   def show
     @user = User.find(current_user.id)
   end
+
+
 end
