e94d73d7b54a86f0b522ae04b97840e93553c7ca
Ginxty <james.oneill96@gmail.com>
2017-10-13 12:08:22 +0100

    lily done, need to adjust sizing though

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.new(first_names: "Ollie", surname: "Holden", start_date: "12-05-2016", course_path: "Engineering", email: 'user@mail.com', password: 'password', password_confirmation: 'password')
+user1 = User.new(first_names: "Ollie", surname: "Holden", start_date: "12-05-2016", course_path: "Engineering", email: 'user@spartaglobal.com', password: 'password', password_confirmation: 'password')
 
 user1.save!
 
-project2 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "http://fillmurray.com/300/300", public_private: true, title:"Project osphellius 1")
-project3 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "http://fillmurray.com/300/300", public_private: true, title:"Project osphellius 1")
-project4 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "http://fillmurray.com/300/300", public_private: true, title:"Project osphellius 1")
-project5 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "http://fillmurray.com/300/300", public_private: true, title:"Project osphellius 1")
-project6 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "http://fillmurray.com/300/300", public_private: true, title:"Project osphellius 1")
-project7 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "http://fillmurray.com/300/300", public_private: true, title:"Project osphellius 1")
-project8 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "http://fillmurray.com/300/300", public_private: true, title:"Project osphellius 1")
-project9 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "http://fillmurray.com/300/300", public_private: true, title:"Project osphellius 1")
-project10 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "http://fillmurray.com/300/300", public_private: true, title:"Project osphellius 1")
+project1 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "Project Link 1", public_private: true, title:"Project osphellius 1")
+project2 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "Project Link 2", public_private: true, title:"Project osphellius 2")
+project3 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "Project Link 3", public_private: true, title:"Project osphellius 3")
+project4 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "Project Link 4", public_private: true, title:"Project osphellius 4")
+project5 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "Project Link 5", public_private: true, title:"Project osphellius 5")
+project6 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "Project Link 6", public_private: true, title:"Project osphellius 6")
+project7 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "Project Link 7", public_private: true, title:"Project osphellius 7")
+project8 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "Project Link 8", public_private: true, title:"Project osphellius 8")
+project9 = Project.create!(screenshot: "http://fillmurray.com/300/300", description: "Desc2", technologies: "JQuery", url: "Project Link 9", public_private: true, title:"Project osphellius 9")
 
 
 
@@ -30,7 +30,7 @@ user1.projects << project6
 user1.projects << project7
 user1.projects << project8
 user1.projects << project9
-user1.projects << project10
+user1.projects << project1
 
 user1.save!
 
