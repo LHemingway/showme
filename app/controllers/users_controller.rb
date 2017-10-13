60abe41ec06e39aee633243c14c72c42b4a63388
= <=>
2017-10-13 12:11:08 +0100

    got the admin to have a users feature which shows all users, delete button path does not work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
 
+# The index for all the users, only to be displayed when you are an admin
+# Achieved by the if statement in navbar; lines 29 & 33
+	def index
+		@users = User.all
+	end
+
 # The show method here is linked to the show page where the spartan can view their own projects
   def show
     @user = User.find(current_user.id)
   end
 
+  def destroy
+  	@user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to "/"
+  end
 
 end
